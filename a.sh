bf30cea85d90f5a9288b2950d875417aef339599
lee <lee-dasilva@hotmail.com>
2022-07-16 19:04:59 +0100

    brightness scrept

--- a.sh
+++ a.sh
@@ -1,49 +1,57 @@
 #!/bin/bash
-# control brightness
-# example ./a.sh -10
-# example ./a.sh +10
+# # control brightness
+# # example ./a.sh -10
+# # example ./a.sh +10
 
-backlight=/sys/class/backlight/amdgpu_bl1/brightness
+# backlight=/sys/class/backlight/amdgpu_bl1/brightness
 
-currentbrightness=(`cat $backlight`)
+# currentbrightness=(`cat $backlight`)
 
-value=`expr $currentbrightness + $1`
-echo $value%
-brightnessctl set $value
+# value=`expr $currentbrightness + $1`
+# echo $value%
+# brightnessctl set $value
 
 # old one not working !
-# MON="eDP"    # Discover monitor name with: xrandr | grep " connected"
-# MON1="HDMI-1"    # Discover monitor name with: xrandr | grep " connected"
-# STEP=5          # Step Up/Down brightnes by: 5 = ".05", 10 = ".10", etc.
+MON="eDP"        # Discover monitor name with: xrandr | grep " connected"
+MON1="HDMI-1"    # Discover monitor name with: xrandr | grep " connected"
+STEP=5           # Step Up/Down brightnes by: 5 = ".05", 10 = ".10", etc.
 
-# CurrBright=$( xrandr --verbose --current | grep ^"$MON" -A5 | tail -n1 )
-# CurrBright="${CurrBright##* }"  # Get brightness level with decimal place
+CurrBright=$( xrandr --verbose --current | grep ^"$MON" -A5 | tail -n1 )
+CurrBright="${CurrBright##* }"  # Get brightness level with decimal place
 
-# Left=${CurrBright%%"."*}        # Extract left of decimal point
-# Right=${CurrBright#*"."}        # Extract right of decimal point
+Left=${CurrBright%%"."*}        # Extract left of decimal point
+Right=${CurrBright#*"."}        # Extract right of decimal point
 
-# MathBright="0"
-# [[ "$Left" != 0 && "$STEP" -lt 10 ]] && STEP=10     # > 1.0, only .1 works
-# [[ "$Left" != 0 ]] && MathBright="$Left"00          # 1.0 becomes "100"
-# [[ "${#Right}" -eq 1 ]] && Right="$Right"0          # 0.5 becomes "50"
-# MathBright=$(( MathBright + Right ))
+MathBright="0"
+[[ "$Left" != 0 && "$STEP" -lt 10 ]] && STEP=10     # > 1.0, only .1 works
+[[ "$Left" != 0 ]] && MathBright="$Left"00          # 1.0 becomes "100"
+[[ "${#Right}" -eq 1 ]] && Right="$Right"0          # 0.5 becomes "50"
+MathBright=$(( MathBright + Right ))
 
-# [[ "$1" == "Up" || "$1" == "+" ]] && MathBright=$(( MathBright + STEP ))
-# [[ "$1" == "Down" || "$1" == "-" ]] && MathBright=$(( MathBright - STEP ))
-# [[ "${MathBright:0:1}" == "-" ]] && MathBright=0    # Negative not allowed
-# [[ "$MathBright" -gt 999  ]] && MathBright=999      # Can't go over 9.99
+[[ "$1" == "Up" || "$1" == "+" ]] && MathBright=$(( MathBright + STEP ))
+[[ "$1" == "Down" || "$1" == "-" ]] && MathBright=$(( MathBright - STEP ))
+[[ "${MathBright:0:1}" == "-" ]] && MathBright=0    # Negative not allowed
+[[ "$MathBright" -gt 999  ]] && MathBright=999      # Can't go over 9.99
 
-# if [[ "${#MathBright}" -eq 3 ]] ; then
-#     MathBright="$MathBright"000         # Pad with lots of zeros
-#     CurrBright="${MathBright:0:1}.${MathBright:1:2}"
-# else
-#     MathBright="$MathBright"000         # Pad with lots of zeros
-#     CurrBright=".${MathBright:0:2}"
-# fi
+if [[ "${#MathBright}" -eq 3 ]] ; then
+    MathBright="$MathBright"000         # Pad with lots of zeros
+    CurrBright="${MathBright:0:1}.${MathBright:1:2}"
+else
+    MathBright="$MathBright"000         # Pad with lots of zeros
+    CurrBright=".${MathBright:0:2}"
+fi
 
-# xrandr --output "$MON" --brightness "$CurrBright"   # Set new brightness
-# xrandr --output "$MON1" --brightness "$CurrBright"
+if [[ $MathBright -lt 10000 ]]
+then
+    exit 0
+elif [[ $MathBright -gt 100000 ]]
+then
+    exit 0
+fi
 
-# # Display current brightness
-# printf "Monitor $MON "
-# echo $( xrandr --verbose --current | grep ^"$MON" -A5 | tail -n1 )
+xrandr --output "$MON" --brightness "$CurrBright"   # Set new brightness
+xrandr --output "$MON1" --brightness "$CurrBright"
+
+# Display current brightness
+printf "Monitor $MON "
+echo $( xrandr --verbose --current | grep ^"$MON" -A5 | tail -n1 )
